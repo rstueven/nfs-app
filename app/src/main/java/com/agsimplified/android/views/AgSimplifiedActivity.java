08c729d6cca9a6fd2dda15b6525f324c58892809
rstueven <rstueven@beerme.com>
2018-05-06 13:54:22 -0500

    Fix permissions and location provider setup

--- app/src/main/java/com/agsimplified/android/views/AgSimplifiedActivity.java
+++ app/src/main/java/com/agsimplified/android/views/AgSimplifiedActivity.java
@@ -1,19 +1,20 @@
 package com.agsimplified.android.views;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 
+import com.agsimplified.android.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -31,7 +32,7 @@ import java.util.List;
  */
 
 abstract class AgSimplifiedActivity extends AppCompatActivity {
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String PERMISSIONS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int REQUEST_FINE_LOCATION = 1;
     private static final String IS_REQUESTING_LOCATION_UPDATES = "isRequestingLocationUpdates";
 
@@ -56,30 +57,70 @@ abstract class AgSimplifiedActivity extends AppCompatActivity {
         // Force landscape orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        checkLocationPermission();
 
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(5000);
+    }
 
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult != null) {
-                    for (Location location : locationResult.getLocations()) {
-                        Log.d("nfs", "LOCATION_RESULT: " + location.toString());
-                        mCurrentLocation = location;
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                final Activity activity = this;
+                new AlertDialog.Builder(activity)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(activity, new String[] {PERMISSIONS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {PERMISSIONS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+            }
+        }
+    }
 
-                        for (LocationListener listener : locationListeners) {
-                            listener.onLocationUpdated(location);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupLocationClient();
+            }
+        }
+    }
+
+    private void setupLocationClient() {
+        if (mFusedLocationClient == null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(5000);
+
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult != null) {
+                        for (Location location : locationResult.getLocations()) {
+                            Log.d("nfs", "LOCATION_RESULT: " + location.toString());
+                            mCurrentLocation = location;
+
+                            for (LocationListener listener : locationListeners) {
+                                listener.onLocationUpdated(location);
+                            }
                         }
                     }
                 }
-            }
-        };
+            };
 
-        checkCurrentLocation();
-        startLocationUpdates();
+            checkCurrentLocation();
+            startLocationUpdates();
+        }
     }
 
     @Override
@@ -136,34 +177,26 @@ abstract class AgSimplifiedActivity extends AppCompatActivity {
         }
     }
 
-    @SuppressLint("MissingPermission")
     protected void startLocationUpdates() {
         Log.d("nfs", "AgSimplifiedActivity.startLocationUpdates()");
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        if (mFusedLocationClient != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSIONS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+            }
+        }
     }
 
     protected void stopLocationUpdates() {
         Log.d("nfs", "AgSimplifiedActivity.stopLocationUpdates()");
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     public void checkCurrentLocation() {
         Log.d("nfs", "AgSimplifiedActivity.checkCurrentLocation()");
         if (mCurrentLocation == null) {
-            if (ContextCompat.checkSelfPermission(this, FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, FINE_LOCATION)) {
-                    Snackbar.make(this.findViewById(android.R.id.content), "Location access required",
-                            Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            // Request the permission
-                            ActivityCompat.requestPermissions(AgSimplifiedActivity.this, new String[]{FINE_LOCATION}, REQUEST_FINE_LOCATION);
-                        }
-                    }).show();
-                } else {
-                    ActivityCompat.requestPermissions(this, new String[]{FINE_LOCATION}, REQUEST_FINE_LOCATION);
-                }
-            } else {
+            if (ContextCompat.checkSelfPermission(this, PERMISSIONS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 loadCurrentLocation();
             }
         }
@@ -171,26 +204,20 @@ abstract class AgSimplifiedActivity extends AppCompatActivity {
 //        onGotCurrentLocation();
     }
 
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        Log.d("nfs", "AgSimplifiedActivity.onRequestPermissionsResult()");
-        if (requestCode == REQUEST_FINE_LOCATION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                loadCurrentLocation();
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
     private void loadCurrentLocation() {
         Log.d("nfs", "AgSimplifiedActivity.loadCurrentLocation()");
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // Got last known location. In some rare situations this can be null.
-                mCurrentLocation = location;
-//                onGotCurrentLocation();
+        if (mFusedLocationClient != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSIONS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        mCurrentLocation = location;
+                        //                onGotCurrentLocation();
+                    }
+                });
             }
-        });
+        }
     }
 
     public Location getCurrentLocation() {
