bfe7d9a23169e0c179fed1b1b65013562e08ac38
rstueven <rstueven@beerme.com>
2018-05-12 15:55:50 -0500

    Refactor into distributionsale and fieldactivity packages

---
+++ app/src/main/java/com/agsimplified/android/views/fieldactivity/FieldActivityDetailsFragment.java
@@ -0,0 +1,43 @@
+package com.agsimplified.android.views.fieldactivity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agsimplified.android.R;
+import com.agsimplified.android.models.fieldactivity.FieldActivity;
+
+public class FieldActivityDetailsFragment extends Fragment {
+    public static FieldActivityDetailsFragment newInstance(FieldActivity fieldActivity) {
+        if (fieldActivity == null) {
+            throw new IllegalArgumentException("null fieldActivity");
+        }
+
+        FieldActivityDetailsFragment frag = new FieldActivityDetailsFragment();
+        Bundle args = new Bundle();
+        args.putSerializable("fieldActivity", fieldActivity);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_field_activity_details, container, false);
+
+        if (savedInstanceState == null) {
+            Bundle args = getArguments();
+            if (args == null) {
+                throw new IllegalStateException("null args");
+            }
+            FieldActivity fieldActivity = (FieldActivity) args.getSerializable("fieldActivity");
+            if (fieldActivity == null) {
+                throw new IllegalStateException("null fieldActivity");
+            }
+        }
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/agsimplified/android/views/fieldactivity/FieldActivitySearchFragment.java
@@ -0,0 +1,122 @@
+package com.agsimplified.android.views.fieldactivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.agsimplified.android.R;
+
+/**
+ * Created by rstueven on 2/28/18.
+ *
+ * Set search parameters.
+ */
+
+public class FieldActivitySearchFragment extends DialogFragment {
+    Spinner clientSelect;
+    Spinner yearSelect;
+    EditText jobCodeView;
+    EditText clientJobCodeView;
+    Spinner typeSelect;
+    Spinner operationSelect;
+    Spinner farmSelect;
+
+    public interface FieldActivitySearcher {
+        void searchFieldActivities(String client, int year, Integer jobCode, Integer clientJobCode, String jobType, String operation, String farm);
+    }
+
+    public FieldActivitySearchFragment() {
+        super();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final FieldActivitySearcher searcher = (FieldActivitySearcher) getActivity();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.fragment_activity_search, null);
+
+        jobCodeView = view.findViewById(R.id.jobCode);
+        clientJobCodeView = view.findViewById(R.id.clientJobCode);
+
+        String[] clients = new String[] {"Barry Farms", "Bary Kienast", "Bedrock Gravel", "Ben Andersen"};
+        clientSelect = view.findViewById(R.id.clientSelect);
+        ArrayAdapter<String> clientAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, clients);
+        clientSelect.setAdapter(clientAdapter);
+
+        String[] years = new String[] {"2018", "2017", "2016", "2015"};
+        yearSelect = view.findViewById(R.id.yearSelect);
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, years);
+        yearSelect.setAdapter(yearAdapter);
+
+        String[] jobTypes = new String[] {"Tillage", "Plant", "Spray", "Fertilizing", "Irrigate", "Other"};
+        typeSelect = view.findViewById(R.id.typeSelect);
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, jobTypes);
+        typeSelect.setAdapter(typeAdapter);
+
+        String[] operations = new String[] {"Tom Kelley", "Wendl Feedlot, Inc.", "Eagle Acres Inc.", "Barry Farms"};
+        operationSelect = view.findViewById(R.id.operationSelect);
+        ArrayAdapter<String> operationAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, operations);
+        operationSelect.setAdapter(operationAdapter);
+
+        String[] farms = new String[] {"Hubbard", "Linda"};
+        farmSelect = view.findViewById(R.id.farmSelect);
+        ArrayAdapter<String> farmAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.spinner_item, farms);
+        farmSelect.setAdapter(farmAdapter);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.search, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        String client = clientSelect.getSelectedItem().toString();
+
+                        int year;
+                        try {
+                            year = Integer.parseInt(yearSelect.getSelectedItem().toString());
+                        } catch (NumberFormatException e) {
+                            year = -1; // This probably isn't right.
+                        }
+
+                        Integer jobCode;
+                        try {
+                            jobCode = Integer.parseInt(jobCodeView.getText().toString());
+                        } catch (NumberFormatException e) {
+                            jobCode = null;
+                        }
+
+                        Integer clientJobCode;
+                        try {
+                            clientJobCode = Integer.parseInt(clientJobCodeView.getText().toString());
+                        } catch (NumberFormatException e) {
+                            clientJobCode = null;
+                        }
+
+                        String jobType = typeSelect.getSelectedItem().toString();
+                        String operation = operationSelect.getSelectedItem().toString();
+                        String farm = farmSelect.getSelectedItem().toString();
+
+                        searcher.searchFieldActivities(client, year, jobCode, clientJobCode, jobType, operation, farm);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        FieldActivitySearchFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
