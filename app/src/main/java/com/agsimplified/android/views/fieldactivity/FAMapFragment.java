c10b66745144d3d1ad3eea48a291c14321346209
rstueven <rstueven@beerme.com>
2018-05-28 18:58:26 -0500

    Store the points along the path.

--- app/src/main/java/com/agsimplified/android/views/fieldactivity/FAMapFragment.java
+++ app/src/main/java/com/agsimplified/android/views/fieldactivity/FAMapFragment.java
@@ -1,6 +1,7 @@
 package com.agsimplified.android.views.fieldactivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,11 +13,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.agsimplified.android.AgSimplified;
 import com.agsimplified.android.R;
 import com.agsimplified.android.models.fieldactivity.FieldActivity;
 import com.agsimplified.android.views.AgSimplifiedActivity;
-import com.agsimplified.android.views.DirectionsFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,6 +23,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +36,8 @@ import java.util.List;
 public class FAMapFragment extends Fragment
         implements OnMapReadyCallback, AgSimplifiedActivity.LocationListener {
     private GoogleMap mMap;
+    private boolean isTrackingLocation = false;
+    private List<Location> path;
 
     public static FAMapFragment newInstance(FieldActivity fieldActivity) {
         Log.d("nfs", "FAMapFragment.newInstance()");
@@ -80,7 +82,12 @@ public class FAMapFragment extends Fragment
             addLoadButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    fakePath();
+                    final AgSimplifiedActivity activity = (AgSimplifiedActivity) getActivity();
+                    if (activity != null) {
+                        activity.registerLocationListener(FAMapFragment.this);
+                        path = new ArrayList<>();
+                        isTrackingLocation = true;
+                    }
                 }
             });
         }
@@ -113,85 +120,91 @@ public class FAMapFragment extends Fragment
                 .zoom(17)
                 .build();
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-//        activity.registerLocationListener(this);
     }
 
     @Override
     public void onLocationUpdated(Location location) {
         Log.d("nfs", "FAMapFragment.onLocationUpdated()");
-        Log.d("nfs", "fieldActivity MAP LOCATION");
-        if (location != null) {
+        if (location != null && isTrackingLocation) {
             Log.d("nfs", location.toString());
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            LatLng newPoint = new LatLng(location.getLatitude(), location.getLongitude());
+            path.add(location);
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(newPoint));
+            if (path.size() > 1) {
+                LatLng oldPoint = new LatLng(path.get(path.size()-2).getLatitude(), path.get(path.size()-2).getLongitude());
+                mMap.addPolyline(new PolylineOptions()
+                        .add(oldPoint, newPoint)
+                        .width(1)
+                        .color(Color.GREEN));
+            }
         }
     }
 
-    private static final List<LatLng> fakePoints = new ArrayList<>();
-    static {
-        fakePoints.add(new LatLng(41.733228, -95.699589));
-        fakePoints.add(new LatLng(41.733405, -95.699441));
-        fakePoints.add(new LatLng(41.733607, -95.699275));
-        fakePoints.add(new LatLng(41.733806, -95.699127));
-        fakePoints.add(new LatLng(41.733913, -95.699026));
-        fakePoints.add(new LatLng(41.734046, -95.698929));
-        fakePoints.add(new LatLng(41.734172, -95.698844));
-        fakePoints.add(new LatLng(41.734273, -95.698743));
-        fakePoints.add(new LatLng(41.734380, -95.698646));
-        fakePoints.add(new LatLng(41.734500, -95.698544));
-        fakePoints.add(new LatLng(41.734566, -95.698413));
-        fakePoints.add(new LatLng(41.734551, -95.698320));
-        fakePoints.add(new LatLng(41.734500, -95.698295));
-        fakePoints.add(new LatLng(41.734437, -95.698341));
-        fakePoints.add(new LatLng(41.734342, -95.698447));
-        fakePoints.add(new LatLng(41.734257, -95.698515));
-        fakePoints.add(new LatLng(41.734140, -95.698599));
-        fakePoints.add(new LatLng(41.734033, -95.698700));
-        fakePoints.add(new LatLng(41.733901, -95.698811));
-        fakePoints.add(new LatLng(41.733790, -95.698887));
-        fakePoints.add(new LatLng(41.733686, -95.698980));
-        fakePoints.add(new LatLng(41.733576, -95.699064));
-        fakePoints.add(new LatLng(41.733488, -95.699140));
-        fakePoints.add(new LatLng(41.733396, -95.699233));
-        fakePoints.add(new LatLng(41.733311, -95.699280));
-        fakePoints.add(new LatLng(41.733226, -95.699220));
-        fakePoints.add(new LatLng(41.733223, -95.699094));
-        fakePoints.add(new LatLng(41.733329, -95.699010));
-        fakePoints.add(new LatLng(41.733408, -95.698929));
-        fakePoints.add(new LatLng(41.733513, -95.698861));
-        fakePoints.add(new LatLng(41.733607, -95.698773));
-        fakePoints.add(new LatLng(41.733715, -95.698688));
-        fakePoints.add(new LatLng(41.733831, -95.698591));
-        fakePoints.add(new LatLng(41.733907, -95.698523));
-        fakePoints.add(new LatLng(41.734058, -95.698413));
-        fakePoints.add(new LatLng(41.734156, -95.698320));
-        fakePoints.add(new LatLng(41.734267, -95.698231));
-        fakePoints.add(new LatLng(41.734368, -95.698147));
-    };
+//    private static final List<LatLng> fakePoints = new ArrayList<>();
+//    static {
+//        fakePoints.add(new LatLng(41.733228, -95.699589));
+//        fakePoints.add(new LatLng(41.733405, -95.699441));
+//        fakePoints.add(new LatLng(41.733607, -95.699275));
+//        fakePoints.add(new LatLng(41.733806, -95.699127));
+//        fakePoints.add(new LatLng(41.733913, -95.699026));
+//        fakePoints.add(new LatLng(41.734046, -95.698929));
+//        fakePoints.add(new LatLng(41.734172, -95.698844));
+//        fakePoints.add(new LatLng(41.734273, -95.698743));
+//        fakePoints.add(new LatLng(41.734380, -95.698646));
+//        fakePoints.add(new LatLng(41.734500, -95.698544));
+//        fakePoints.add(new LatLng(41.734566, -95.698413));
+//        fakePoints.add(new LatLng(41.734551, -95.698320));
+//        fakePoints.add(new LatLng(41.734500, -95.698295));
+//        fakePoints.add(new LatLng(41.734437, -95.698341));
+//        fakePoints.add(new LatLng(41.734342, -95.698447));
+//        fakePoints.add(new LatLng(41.734257, -95.698515));
+//        fakePoints.add(new LatLng(41.734140, -95.698599));
+//        fakePoints.add(new LatLng(41.734033, -95.698700));
+//        fakePoints.add(new LatLng(41.733901, -95.698811));
+//        fakePoints.add(new LatLng(41.733790, -95.698887));
+//        fakePoints.add(new LatLng(41.733686, -95.698980));
+//        fakePoints.add(new LatLng(41.733576, -95.699064));
+//        fakePoints.add(new LatLng(41.733488, -95.699140));
+//        fakePoints.add(new LatLng(41.733396, -95.699233));
+//        fakePoints.add(new LatLng(41.733311, -95.699280));
+//        fakePoints.add(new LatLng(41.733226, -95.699220));
+//        fakePoints.add(new LatLng(41.733223, -95.699094));
+//        fakePoints.add(new LatLng(41.733329, -95.699010));
+//        fakePoints.add(new LatLng(41.733408, -95.698929));
+//        fakePoints.add(new LatLng(41.733513, -95.698861));
+//        fakePoints.add(new LatLng(41.733607, -95.698773));
+//        fakePoints.add(new LatLng(41.733715, -95.698688));
+//        fakePoints.add(new LatLng(41.733831, -95.698591));
+//        fakePoints.add(new LatLng(41.733907, -95.698523));
+//        fakePoints.add(new LatLng(41.734058, -95.698413));
+//        fakePoints.add(new LatLng(41.734156, -95.698320));
+//        fakePoints.add(new LatLng(41.734267, -95.698231));
+//        fakePoints.add(new LatLng(41.734368, -95.698147));
+//    };
 
-    public void fakePath() {
-        final AgSimplifiedActivity activity = (AgSimplifiedActivity) getActivity();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (LatLng p : fakePoints) {
-                    final Location l = new Location("fake");
-                    l.setLatitude(p.latitude);
-                    l.setLongitude(p.longitude);
-                    Log.d("nfs", "FAKE: " + l.toString());
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(l.getLatitude(), l.getLongitude())));
-                        }
-                    });
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-    }
+//    public void fakePath() {
+//        final AgSimplifiedActivity activity = (AgSimplifiedActivity) getActivity();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (LatLng p : fakePoints) {
+//                    final Location l = new Location("fake");
+//                    l.setLatitude(p.latitude);
+//                    l.setLongitude(p.longitude);
+//                    Log.d("nfs", "FAKE: " + l.toString());
+//                    activity.runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(l.getLatitude(), l.getLongitude())));
+//                        }
+//                    });
+//                    try {
+//                        Thread.sleep(5000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }).start();
+//    }
 }
